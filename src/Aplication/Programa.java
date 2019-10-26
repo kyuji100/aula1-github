b660abc81d0f2908798b9f1c9e007dd636393314
Yuji Fujimoto <adm_yuji@hotmail.com>
2019-10-26 13:14:52 -0300

    Adicionado frases

---
+++ src/Aplication/Programa.java
@@ -0,0 +1,14 @@
+package Aplication;
+
+public class Programa {
+
+	public static void main(String[] args) {
+		System.out.println("Bom dia!");
+		System.out.println("Boa tarde!");
+		System.out.println("Boa noite!");
+		System.out.println("Sainara");
+		System.out.println("Arigato");
+
+	}
+
+}
--- src/module-info.java
+++
@@ -1,9 +0,0 @@
-/**
- * 
- */
-/**
- * @author Yuji
- *
- */
-module GITHUB {
-}
